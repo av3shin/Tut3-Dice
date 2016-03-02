d7adb2785348c6bf132cd6f3d8e7f344a727be79
Aveshin Naidoo <av3shin@gmail.com>
2016-03-02 15:28:51 +0200

    Added Average

--- DicePrac3/DicePrac3/DiceRoller.cpp
+++ DicePrac3/DicePrac3/DiceRoller.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <ctime>
 
+using namespace std;
+
 class Dice
 {
 public:
 	Dice();
 	~Dice();
 	int getNum();
+	void print();
+	float average(Dice, int);
+	float average(int[], int);
+
 
 private:
 
@@ -14,10 +20,18 @@ private:
 
 Dice::Dice()
 {
+		srand(time(NULL));
 }
 
 Dice::~Dice()
 {
+
+	srand(time(NULL));
+}
+
+float Dice::average(Dice roll, int num)
+{
+
 }
 
 int Dice::getNum()
@@ -25,8 +39,18 @@ int Dice::getNum()
 		return (rand() % 6) + 1; //Random number generator in range [1-6]
 }
 
+void Dice::print()
+{
+	int num;
+	num = getNum();
+	cout << num << endl;
+
+}
 
 int main()
 {
+	Dice X;
+	X.print();
+	return 0;
 
 }
